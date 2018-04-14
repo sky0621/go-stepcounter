cb5561977b208c118f53cc3c7b3763239c93467d
sky0621 <sky14work@gmail.com>
2018-04-14 22:46:26 +0900

    update template.go

--- _sampleresult/result.md
+++ _sampleresult/result.md
@@ -1,13 +1,13 @@
-# 各ソースステップ数一覧(2018-04-14 22:39 時点)
+# 各ソースステップ数一覧(2018-04-14 22:46 時点)
 
 #### ※ツール（ https://github.com/sky0621/go-stepcounter ）による自動生成
 
-Total
-
-| TotalStep | 82 |
-| TotalComment | 12 |
+| TotalStep | TotalComment |
+| :--- | :--- |
+| 82 | 12 |
 
 | Path | Step | Comment |
+| :--- | :--- | :--- |
 | _sampleproject/app.go | 8 | 0 |
 | _sampleproject/client/aws/sqs.go | 6 | 5 |
 | _sampleproject/cmd/main.go | 36 | 0 |
--- template.go
+++ template.go
@@ -6,23 +6,23 @@ import (
 	"github.com/jessevdk/go-assets"
 )
 
-var _Assets95aa4ed3a0a5918c330d9340efd86426339c5692 = "# 各ソースステップ数一覧({{.Datetime}} 時点)\n\n#### ※ツール（ https://github.com/sky0621/go-stepcounter ）による自動生成\n\nTotal\n\n| TotalStep | {{.TotalStep}} |\n| TotalComment | {{.TotalComment}} |\n\n| Path | Step | Comment |\n{{range .FileStepCounters}}| {{.FilePath}} | {{.Step}} | {{.Comment}} |\n{{end}}\n"
+var _Assets95aa4ed3a0a5918c330d9340efd86426339c5692 = "# 各ソースステップ数一覧({{.Datetime}} 時点)\n\n#### ※ツール（ https://github.com/sky0621/go-stepcounter ）による自動生成\n\n| TotalStep | TotalComment |\n| :--- | :--- |\n| {{.TotalStep}} | {{.TotalComment}} |\n\n| Path | Step | Comment |\n| :--- | :--- | :--- |\n{{range .FileStepCounters}}| {{.FilePath}} | {{.Step}} | {{.Comment}} |\n{{end}}\n"
 
 // Assets returns go-assets FileSystem
-var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"tmpl"}, "/tmpl": []string{"eachSource.md"}}, map[string]*assets.File{
-	"/": &assets.File{
+var Assets = assets.NewFileSystem(map[string][]string{"/tmpl": []string{"eachSource.md"}, "/": []string{"tmpl"}}, map[string]*assets.File{
+	"/tmpl/eachSource.md": &assets.File{
+		Path:     "/tmpl/eachSource.md",
+		FileMode: 0x1b4,
+		Mtime:    time.Unix(1523713113, 1523713113758159374),
+		Data:     []byte(_Assets95aa4ed3a0a5918c330d9340efd86426339c5692),
+	}, "/": &assets.File{
 		Path:     "/",
 		FileMode: 0x800001ed,
-		Mtime:    time.Unix(1523709893, 1523709893798453855),
+		Mtime:    time.Unix(1523712740, 1523712740738858023),
 		Data:     nil,
 	}, "/tmpl": &assets.File{
 		Path:     "/tmpl",
 		FileMode: 0x800001ed,
 		Mtime:    time.Unix(1523712493, 1523712493430865905),
 		Data:     nil,
-	}, "/tmpl/eachSource.md": &assets.File{
-		Path:     "/tmpl/eachSource.md",
-		FileMode: 0x1b4,
-		Mtime:    time.Unix(1523712471, 1523712471743544939),
-		Data:     []byte(_Assets95aa4ed3a0a5918c330d9340efd86426339c5692),
 	}}, "")
